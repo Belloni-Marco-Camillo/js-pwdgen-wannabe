b6e6bd5c8c2d2c370c12eb2f556fba364f06f745
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-03-23 14:22:50 +0100

    end-commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,3 +2,4 @@ const nome_utente = prompt('inserisci il tuo nome');
 const cognome_utente = prompt('inserisci il tuo cognome');
 const colore = prompt('inserisci il tuo colore preferito');
 const password = nome_utente + cognome_utente + colore + 22;
+document.getElementById('psw').innerHTML = password;
